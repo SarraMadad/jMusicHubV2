d7e60931853bb0058b5b8b3528808214a0d63a7b
Louaty <houdalouaty@yahoo.com>
2021-04-02 21:22:22 +0200

    Test Client

--- src/main/java/musichub/util/Server.java
+++ src/main/java/musichub/util/Server.java
@@ -1,28 +1,67 @@
 package musichub.util;
-
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
-
+import java.util.Scanner;
+/*
+ * www.projetSNIR_LOUATY.com
+ */
 public class Server {
 
-    public static int PORT = 9090;
-    public static void main (String[] args) throws IOException {
-        ServerSocket listener = new ServerSocket (PORT);
-        System.out.println("[SERVER] Waiting for client connect...");
-        Socket client = listener.accept();
-        System.out.println("[SERVER] Connected to client");
+    public static void main(String[] test) {
 
-        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-        String date = new Date().toString();
-        System.out.println("[SERVER] Sent Date " + date);
-        out.println(date);
+        final ServerSocket serveurSocket  ;
+        final Socket clientSocket ;
+        final BufferedReader in;
+        final PrintWriter out;
+        final Scanner sc=new Scanner(System.in);
 
-        System.out.println("[SERVER] Sent Date. Closing...");
-        client.close();
-        listener.close();
+        try {
+            serveurSocket = new ServerSocket(5000);
+            clientSocket = serveurSocket.accept();
+            out = new PrintWriter(clientSocket.getOutputStream());
+            in = new BufferedReader (new InputStreamReader (clientSocket.getInputStream()));
+            Thread envoi= new Thread(new Runnable() {
+                String msg;
+                @Override
+                public void run() {
+                    while(true){
+                        msg = sc.nextLine();
+                        out.println(msg);
+                        out.flush();
+                    }
+                }
+            });
+            envoi.start();
+
+            Thread recevoir= new Thread(new Runnable() {
+                String msg ;
+                @Override
+                public void run() {
+                    try {
+                        msg = in.readLine();
+                        //tant que le client est connecté
+                        while(msg!=null){
+                            System.out.println("Client : "+msg);
+                            msg = in.readLine();
+                        }
+                        //sortir de la boucle si le client a déconecté
+                        System.out.println("Client déconecté");
+                        //fermer le flux et la session socket
+                        out.close();
+                        clientSocket.close();
+                        serveurSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            recevoir.start();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
